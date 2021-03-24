0875ff17909d058749771f3330f5c931aab3c551
gar622 <73448556+gar622@users.noreply.github.com>
2021-03-24 14:18:41 -0600

    commit two
    table is created with bulma -- trying to link existing data? or is it manually done? idk yet

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ async function bootServer() {
   try {
     const mysql = await db.sequelizeDB;
     await mysql.sync();
+    app.use(express.static('public'));
     app.listen(PORT, () => {
       console.log(`Listening on: http//localhost:${PORT}`);
     });
