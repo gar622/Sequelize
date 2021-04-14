89a6e3612bc9051208e972db544c49b6e47962aa
gar622 <73448556+gar622@users.noreply.github.com>
2021-04-14 12:51:01 -0600

    update server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import express from 'express';
 import db from './database/initializeDB.js';
 import apiRoutes from './routes/apiRoutes.js';
@@ -5,21 +6,18 @@ import apiRoutes from './routes/apiRoutes.js';
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
 const staticFolder = 'public';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(staticFolder));
 
-//app.use(express.static('public'));
 app.use('/api', apiRoutes);
 
 async function bootServer() {
   try {
     const mysql = await db.sequelizeDB;
     await mysql.sync();
-    app.use(express.static('public'));
     app.listen(PORT, () => {
       console.log(`Listening on: http//localhost:${PORT}`);
     });
